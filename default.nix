d475c44577084a25e46f60793fc9e88b59d24445
sam bacha <sam@freighttrust.com>
2021-02-26 14:35:01 -0800

    nix

--- default.nix
+++ default.nix
@@ -1,1 +1,8 @@
-(import ./nix { }).ci
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.python3Packages.buildPythonApplication {
+  pname = "app";
+  src = ./module.;
+  version = "0.1.0";
+  propagatedBuildInputs = [ pkgs.python3Packages.flask ];
+}
